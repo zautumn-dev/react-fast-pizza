16f8ff96b4b712a17acddd2501bc8521393a7236
lupinus <bzirs@outlook.com>
2025-10-15 19:03:57 +0800

    ✨ feat: tailwind Loading 样式

--- src/shared/ui/Loader.jsx
+++ src/shared/ui/Loader.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 function Loader() {
-  return <div className="loader"></div>
+  return (
+    //  inset 位置 设置
+    // https://tailwindcss.com/docs/top-right-bottom-left#basic-example
+    // backdrop-blur 模糊
+    // https://tailwindcss.com/docs/backdrop-filter-blur#using-a-custom-value
+    <div className="absolute inset-0 flex items-center justify-center bg-slate-200/40 backdrop-blur-sm">
+      <div className="loader"></div>
+    </div>
+  )
 }
 
 export default Loader
