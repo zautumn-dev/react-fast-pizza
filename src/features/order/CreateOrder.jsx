1bb9636982f4870500469ec1207e98ed5e0603a3
lupinus <bzirs@outlook.com>
2025-10-18 18:25:51 +0800

    ✨ feat: tailwind 订单相关样式

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -41,42 +41,48 @@ function CreateOrder() {
   console.log(error)
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       <Form method="post">
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+
+          <input type="text" name="customer" required className="input grow" />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+
+          <div className="grow">
+            <input type="tel" name="phone" required className="input w-full" />
           </div>
           {error && error.phone && `${error.phone}`}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required className="input" />
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+
+          <div className="grow">
+            <input type="text" name="address" required className="input w-full" />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           {/* accent */}
           {/*https://tailwindcss.com/docs/accent-color*/}
           <input
             type="checkbox"
             name="priority"
             id="priority"
-            className="h-6 w-6 accent-yellow-400 focus:ring-3 focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none"
+            className="h-6 w-6 accent-yellow-400 focus:ring focus:ring-yellow-400 focus:ring-offset-2 focus:outline-none"
+
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
